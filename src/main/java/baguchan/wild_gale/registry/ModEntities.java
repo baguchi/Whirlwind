7e461f6c343386bfdbf077ef9669ce92cc3fd9b7
baguchan <shutux500@gmail.com>
2023-12-17 15:35:37 +0900

    1.20.4-1.4.0

--- src/main/java/baguchan/wild_gale/registry/ModEntities.java
+++ src/main/java/baguchan/wild_gale/registry/ModEntities.java
@@ -6,6 +6,7 @@ import baguchan.wild_gale.entity.WhirlWind;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.flag.FeatureFlags;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
@@ -18,8 +19,8 @@ public class ModEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES_REGISTRY = DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, WhirlWindMod.MODID);
 
 
-    public static final Supplier<EntityType<WhirlWind>> WHIRLWIND = ENTITIES_REGISTRY.register("whirl_wind", () -> EntityType.Builder.of(WhirlWind::new, MobCategory.MONSTER).sized(0.8F, 2.0F).clientTrackingRange(8).build(prefix("whirl_wind")));
-    public static final Supplier<EntityType<ChargePotion>> CHARGE_POTION = ENTITIES_REGISTRY.register("charge_potion", () -> EntityType.Builder.<ChargePotion>of(ChargePotion::new, MobCategory.MISC).sized(0.3F, 0.3F).clientTrackingRange(10).build(prefix("charge_potion")));
+    public static final Supplier<EntityType<WhirlWind>> WHIRLWIND = ENTITIES_REGISTRY.register("whirl_wind", () -> EntityType.Builder.of(WhirlWind::new, MobCategory.MONSTER).sized(0.8F, 2.0F).clientTrackingRange(8).requiredFeatures(FeatureFlags.UPDATE_1_21).build(prefix("whirl_wind")));
+    public static final Supplier<EntityType<ChargePotion>> CHARGE_POTION = ENTITIES_REGISTRY.register("charge_potion", () -> EntityType.Builder.<ChargePotion>of(ChargePotion::new, MobCategory.MISC).sized(0.3F, 0.3F).clientTrackingRange(10).requiredFeatures(FeatureFlags.UPDATE_1_21).build(prefix("charge_potion")));
 
     @SubscribeEvent
     public static void registerEntityAttribute(EntityAttributeCreationEvent event) {
